d0a825fd03d07d9fe80360a035e510138d64f4fd
jordan fischer <jordan fischer>
2016-11-26 10:07:42 +1000

    Registration process finished and debugged

--- webservice/core.php
+++ webservice/core.php
@@ -27,17 +27,25 @@
 		}
 		
 		public function register($username, $email, $password, $confirmPassword) {
-			if(preg_match('^[a-zA-Z0-9]+$', $username) === false) {
+			if(preg_match('/^[a-zA-Z0-9]+$/', $username) === false) {
 				$this->errors[] = "Username must be only letters and numbers";
 			}
 			if($this->validate($email, 'email') === false) {
 				$this->errors[] = "You must enter a valid email address";
 			}
-			if(preg_match('^[a-zA-Z]\w{3,14}$', $password) === false) {
+			if(preg_match('/^[a-zA-Z]\w{3,14}$/', $password) === false) {
 				$this->errors[] = "Your password must only contain letters, numbers and underscores and be between 4 and 15 charecters.";
 			}
 			if($password != $confirmPassword) {
 				$this->errors[] = "The two passwords you have enetered do not match.";
+			}
+			if(empty($this->errors)) {
+				$stmt = $this->pdo->prepare("SELECT * FROM `user` WHERE `email` = ? LIMIT 0,1");
+				$stmt->execute(array($email));
+				$result = $stmt->fetch();
+				if(empty($result) === false) {
+					$this->errors[] = "That email already exists in our database.";
+				}
 			}
 			$password = password_hash($password, PASSWORD_DEFAULT);
 			if(empty($this->errors)) {
