d772a2a17b02bf8c4f90572b5d73794c42e32249
jp <jp@localhost.localdomain>
2016-11-26 13:09:59 +1000

    compass updates

--- compass/compass.js
+++ compass/compass.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 
     if (window.DeviceOrientationEvent) {
-        document.getElementById("notice").innerHTML = "Gaaf! De DeviceOrientationEvent API word door dit toestel ondersteund.";
         window.addEventListener('deviceorientation', function(eventData) {
             // gamma: Tilting the device from left to right. Tilting the device to the right will result in a positive value.
             // gamma: Het kantelen van links naar rechts in graden. Naar rechts kantelen zal een positieve waarde geven.
@@ -13,7 +12,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
             // alpha: The direction the compass of the device aims to in degrees.
             // alpha: De richting waarin de kompas van het apparaat heen wijst in graden.
-            var dir = eventData.alpha
+            //var dir = eventData.alpha
+            var dir = eventData.alpha * 180 / Math.PI;
 
             // Call the function to use the data on the page.
             // Roep de functie op om de data op de pagina te gebruiken.
